6e982384c812b653e1675b3f0abb7aa396e9a1bb
Harry <harryrock67@gmail.com>
2018-12-19 15:31:18 -0500

    check

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,8 +1,7 @@
-let sketch = new Sketch(),
-    scene, renderer, camera, canvas;
+var sketch = new Sketch(),
+    scene, renderer, camera;
 
 const init = () => {
-    canvas = document.getElementById('canvas');
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
     renderer = new THREE.WebGLRenderer({
@@ -10,26 +9,21 @@ const init = () => {
         alpha: true
     });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    canvas.appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement);
     window.addEventListener("resize", () => {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
         renderer.setSize(window.innerWidth, window.innerHeight);
     }, false);
-    document.body.appendChild(canvas);
+    scene.add(camera);
     sketch.init();
     animate();
 }
 
 const animate = () => {
     sketch.update();
-    render();
-    requestAnimationFrame(animate);
-}
-
-const render = () => {
-    renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
     renderer.render(scene, camera);
+    requestAnimationFrame(animate);
 }
 
 init();
