b5d8f0622c51370831916d443237392da8a132fe
Harry <harryrock67@gmail.com>
2018-12-19 16:57:46 -0500

    For loop for boxes

--- dev/js/sketch.js
+++ dev/js/sketch.js
@@ -1,11 +1,17 @@
-const initBoxes = () {
-    for (let i = 0; i < 24; i++) {
-        for (let j = 0; j < 24; j++) {
-
+const initBoxes = () => {
+    geo = new THREE.BoxGeometry(1, 1, 1, 10);
+    mat = new THREE.MeshPhongMaterial({
+        color: 0xe62929,
+        wireframe: true
+    });
+    mesh = new THREE.Mesh(geo, mat);
+    for (let x = 0; x < 24; x++) {
+        for (let y = 0; y < 24; y++) {
+            mesh.position.set(x, 0.5, y);
+            createMesh();
         }
-        console.log(i);
     }
-    console.log('hey');
+    clearRam('all');
 }
 
 class Sketch {
