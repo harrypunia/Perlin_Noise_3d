9cf6c2aedeb29124420a6b2064dad914453aaf7e
Harry <harryrock67@gmail.com>
2018-12-19 18:22:00 -0500

    Glow if above certain value

--- dev/js/environment.js
+++ dev/js/environment.js
@@ -1,12 +1,11 @@
-const initEnvironment = () => {
-    geo = new THREE.PlaneGeometry(80, 80, 25, 25);
-    mat = new THREE.MeshBasicMaterial({
-        color: 0xff6767,
-        side: THREE.DoubleSide
-        //wireframe: true
-    })
-    mesh = new THREE.Mesh(geo, mat);
-    mesh.rotation.x = Math.PI / 2;
-    floor = mesh;
-    createMesh('clearRam');
+//const initEnvironment = () => {
+//    geo = new THREE.PlaneGeometry(80, 80, 25, 25);
+//    mat = new THREE.MeshBasicMaterial({
+//        //color: 0xff6767,
+//        //wireframe: true
+//    })
+//    mesh = new THREE.Mesh(geo, mat);
+//    mesh.rotation.x = Math.PI / 2;
+//    floor = mesh;
+//    createMesh('clearRam');
 }
--- dev/js/sketch.js
+++ dev/js/sketch.js
@@ -1,13 +1,13 @@
 const initBoxes = () => {
-    geo = new THREE.BoxGeometry(3.9, 3.9, 3.9);
-    mat = new THREE.MeshBasicMaterial({
-        color: 0x000000,
-        //wireframe: true
-    });
+    geo = new THREE.BoxGeometry(4, 4, 4);
     for (let x = 0; x < 100; x += 4) {
         for (let y = 0; y < 100; y += 4) {
+            mat = new THREE.MeshBasicMaterial({
+                color: 0xffffff,
+                wireframe: true
+            });
             mesh = new THREE.Mesh(geo, mat);
-            mesh.position.set(x - 48, 0, y - 48);
+            mesh.position.set(x - 48, 4, y - 48);
             mesh.px = x;
             mesh.py = y;
             boxes.push(mesh);
@@ -19,17 +19,18 @@ const initBoxes = () => {
 const updateBoxes = () => {
     for (let i in boxes) {
         let value = Math.abs(noise.perlin2(boxes[i].px / 100, boxes[i].py / 100)) * 10;
-        value -= 2;
+        value -= 4;
         boxes[i].position.y = value;
         boxes[i].px += perlin.freq;
         boxes[i].py += perlin.freq;
+        boxes[i].material.color.setRGB(value, 0, value / 4);
     }
 }
 
 class Sketch {
     constrctor() {}
     init() {
-        initEnvironment();
+        //initEnvironment();
         initBoxes();
     }
     update() {
