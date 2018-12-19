626ec12f1ada5b56f67f55c40c114e067b6f914c
Harry <harryrock67@gmail.com>
2018-12-19 17:54:04 -0500

    Boxes and plain algined

--- dev/js/environment.js
+++ dev/js/environment.js
@@ -1,8 +1,9 @@
 const initEnvironment = () => {
     geo = new THREE.PlaneGeometry(100, 100, 25, 25);
     mat = new THREE.MeshBasicMaterial({
-        color: 0x212121,
-        wireframe: true
+        color: 0x6796ff,
+        side: THREE.DoubleSide
+        //wireframe: true
     })
     mesh = new THREE.Mesh(geo, mat);
     mesh.rotation.x = Math.PI / 2;
--- dev/js/sketch.js
+++ dev/js/sketch.js
@@ -1,13 +1,13 @@
 const initBoxes = () => {
     geo = new THREE.BoxGeometry(4, 4, 4);
     mat = new THREE.MeshBasicMaterial({
-        color: 0xe62929,
-        wireframe: true
+        color: 0x448e6c,
+        //wireframe: true
     });
     for (let x = 0; x < 100; x += 4) {
         for (let y = 0; y < 100; y += 4) {
             mesh = new THREE.Mesh(geo, mat);
-            mesh.position.set(x - 50, 2, y - 50);
+            mesh.position.set(x - 48, 2, y - 48);
             mesh.px = x;
             mesh.py = y;
             boxes.push(mesh);
